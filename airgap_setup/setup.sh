3cad2f64b1ea250aeb58e0ffff056b2636fc250e
Vasiliy Shapovalov <vsh@MacBook-Pro-Vasiliy.local>
2020-12-04 13:24:40 +0300

    airgap setup

---
+++ airgap_setup/setup.sh
@@ -0,0 +1,5 @@
+wget https://ftp.mozilla.org/pub/firefox/releases/83.0/linux-x86_64/en-US/firefox-83.0.tar.bz2
+wget https://github.com/lidofinance/dc4bc/releases/download/0.1.0/dc4bc_airgapped_linux
+sha1sum firefox-83.0.tar.bz2
+cp ../qr_reader_bundle/index.html ./index.html
+mv dc4bc_airgapped_linux dc4bc_airgapped
