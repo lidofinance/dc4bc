eb5a7f88789c9b04243ace962bd50230a5d413d1
Andrej Zavgorodnij <a.zavgorodny@corp.mail.ru>
2020-12-07 13:10:29 +0300

    updated checksums

--- airgapped_setup/setup.sh
+++ airgapped_setup/setup.sh
@@ -3,13 +3,13 @@
 wget -q --show-progress https://ftp.mozilla.org/pub/firefox/releases/83.0/linux-x86_64/en-US/firefox-83.0.tar.bz2
 shasum firefox-83.0.tar.bz2 |
 while read -r sum _ ; do
-  [[ $sum == 19asdasdasd56462e44d61a093ea57e964cf0af05c0e ]] && echo "Firefox checksum is correct." || echo "[WARN] Incorrect Firefox checksum!"
+  [[ $sum == 43a0fb57ce1bfd348a15bbcc092ac7cced79ce79 ]] && echo "Firefox checksum is correct." || echo "[WARN] Incorrect Firefox checksum!"
 done
 
-wget -q --show-progress https://github.com/lidofinance/dc4bc/releases/download/0.1.0/dc4bc_airgapped_linux
+wget -q --show-progress https://github.com/lidofinance/dc4bc/releases/download/0.1.1/dc4bc_airgapped_linux
 shasum dc4bc_airgapped_linux |
 while read -r sum _ ; do
-  [[ $sum == 19asdasdasd56462e44d61a093ea57e964cf0af05c0e ]] && echo "Airgapped checksum is correct." || echo "[WARN] Incorrect Airgapped checksum!"
+  [[ $sum == 0dd1aed350f4d58957dd4ebf58d845586db2cac3 ]] && echo "Airgapped checksum is correct." || echo "[WARN] Incorrect Airgapped checksum!"
 done
 
 cp ../qr_reader_bundle/index.html ./index.html
