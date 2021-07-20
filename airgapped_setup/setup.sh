8de3af6532d2172c1fe117889509725f2be51c09
Mike Mozhaev <programmer10110@yandex.ru>
2021-07-20 14:21:37 +0300

    Update setup.sh

--- airgapped_setup/setup.sh
+++ airgapped_setup/setup.sh
@@ -6,10 +6,10 @@ while read -r sum _ ; do
   [[ $sum == 43a0fb57ce1bfd348a15bbcc092ac7cced79ce79 ]] && echo "Firefox checksum is correct." || echo "[WARN] Incorrect Firefox checksum!"
 done
 
-wget -q --show-progress https://github.com/lidofinance/dc4bc/releases/download/0.1.4/dc4bc_airgapped_linux
+wget -q --show-progress https://github.com/lidofinance/dc4bc/releases/download/2.0.0/dc4bc_airgapped_linux
 shasum dc4bc_airgapped_linux |
 while read -r sum _ ; do
-  [[ $sum == 791a319d9e77622dc80ca04553a410ae1198ae11 ]] && echo "Airgapped checksum is correct." || echo "[WARN] Incorrect Airgapped checksum!"
+  [[ $sum == 11f5bf387f185c36f5d7e04bd6f1efba71f54596 ]] && echo "Airgapped checksum is correct." || echo "[WARN] Incorrect Airgapped checksum!"
 done
 
 cp ../qr_reader_bundle/index.html ./index.html
