99cde6b19f43af5845503d973aaf885ca7e4a84b
Sergio Sotnikov <ssotnikov93@gmail.com>
2021-10-26 14:54:03 +0300

    move http requests handling to new ContextService methods #LSC-134

--- client/api/http_api/handlers/fsm.go
+++ client/api/http_api/handlers/fsm.go
@@ -1,11 +1,8 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
 
-	"github.com/censync/go-dto"
-	"github.com/censync/go-validator"
 	"github.com/labstack/echo/v4"
 	. "github.com/lidofinance/dc4bc/client/api/dto"
 	cs "github.com/lidofinance/dc4bc/client/api/http_api/context_service"
@@ -14,65 +11,23 @@ import (
 
 func (a *HTTPApp) GetFSMDump(c echo.Context) error {
 	stx := c.(*cs.ContextService)
-
-	request := &req.DkgIdForm{}
-
-	err := stx.Bind(request)
-
-	if err != nil {
-		return stx.JsonError(
-			http.StatusBadRequest,
-			fmt.Errorf("failed to read request body: %v", err),
-		)
-	}
-
-	if err := validator.Validate(request); !err.IsEmpty() {
-		return stx.JsonError(
-			http.StatusBadRequest,
-			err.Error(),
-		)
-	}
-
 	formDTO := &DkgIdDTO{}
-
-	err = dto.RequestToDTO(formDTO, request)
-
-	if err != nil {
-		return stx.JsonError(
-			http.StatusBadRequest,
-			err,
-		)
+	if err := stx.BindToDTO(&req.DkgIdForm{}, formDTO); err != nil {
+		return err
 	}
 
 	fsmDump, err := a.node.GetFSMDump(formDTO)
-
-	if err == nil {
-		return stx.Json(
-			http.StatusOK,
-			fsmDump,
-		)
-	} else {
-		return stx.JsonError(
-			http.StatusInternalServerError,
-			err,
-		)
+	if err != nil {
+		return stx.JsonError(http.StatusInternalServerError, err)
 	}
+	return stx.Json(http.StatusOK, fsmDump)
 }
 
 func (a *HTTPApp) GetFSMList(c echo.Context) error {
 	stx := c.(*cs.ContextService)
-
 	fsmDump, err := a.node.GetFSMList()
-
-	if err == nil {
-		return stx.Json(
-			http.StatusOK,
-			fsmDump,
-		)
-	} else {
-		return stx.JsonError(
-			http.StatusInternalServerError,
-			err,
-		)
+	if err != nil {
+		return stx.JsonError(http.StatusInternalServerError, err)
 	}
+	return stx.Json(http.StatusOK, fsmDump)
 }
