e88afededd6e5c238b647bf4df2eb35a99499828
Sergio Sotnikov <ssotnikov93@gmail.com>
2021-10-26 14:53:07 +0300

    add BindToRequest and BindToDTO methods for ContextService #LSC-134

--- client/api/http_api/context_service/context_service.go
+++ client/api/http_api/context_service/context_service.go
@@ -2,7 +2,10 @@ package context_service
 
 import (
 	"fmt"
+	"net/http"
 
+	"github.com/censync/go-dto"
+	"github.com/censync/go-validator"
 	"github.com/labstack/echo/v4"
 )
 
@@ -31,6 +34,29 @@ func (e *CSErrorResp) Error() string {
 		return ""
 	}
 	return fmt.Sprintf("%s", e.ErrorMessage)
+}
+
+// BindToRequest populates the request fields based on the context path and query parameters and body
+// and validates the result.
+func (cs *ContextService) BindToRequest(request interface{}) error {
+	if err := cs.Bind(request); err != nil {
+		return cs.JsonError(http.StatusBadRequest, fmt.Errorf("failed to read request body: %v", err))
+	}
+	if err := validator.Validate(request); !err.IsEmpty() {
+		return cs.JsonError(http.StatusBadRequest, err.Error())
+	}
+	return nil
+}
+
+// BindToDTO builds a request of the given form based on the context and converts it to a DTO.
+func (cs *ContextService) BindToDTO(requestForm, dtoForm interface{}) error {
+	if err := cs.BindToRequest(requestForm); err != nil {
+		return err
+	}
+	if err := dto.RequestToDTO(dtoForm, requestForm); err != nil {
+		return cs.JsonError(http.StatusBadRequest, err)
+	}
+	return nil
 }
 
 func (cs *ContextService) Json(code int, data interface{}) error {
