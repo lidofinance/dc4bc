4b3509096ff287d56498c2f5ad537b2dd0e0d76b
pakgw <pakgw@xplanet.ru>
2021-10-27 12:11:07 +0300

    uncommented test

--- client/services/node/node_service_test.go
+++ client/services/node/node_service_test.go
@@ -2,17 +2,24 @@ package node
 
 import (
 	"context"
+	"crypto/ed25519"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"testing"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/lidofinance/dc4bc/client/api/dto"
 	"github.com/lidofinance/dc4bc/client/config"
 	"github.com/lidofinance/dc4bc/client/modules/keystore"
 	"github.com/lidofinance/dc4bc/client/modules/logger"
 	"github.com/lidofinance/dc4bc/client/services"
 	"github.com/lidofinance/dc4bc/client/types"
+	"github.com/lidofinance/dc4bc/fsm/state_machines"
+	spf "github.com/lidofinance/dc4bc/fsm/state_machines/signature_proposal_fsm"
+	"github.com/lidofinance/dc4bc/fsm/types/requests"
+	"github.com/lidofinance/dc4bc/storage"
 
 	"github.com/golang/mock/gomock"
 	"github.com/lidofinance/dc4bc/mocks/clientMocks"
@@ -20,92 +27,90 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// func TestClient_ProcessMessage(t *testing.T) {
-// 	var (
-// 		ctx  = context.Background()
-// 		req  = require.New(t)
-// 		ctrl = gomock.NewController(t)
-// 	)
-// 	defer ctrl.Finish()
+func TestClient_ProcessMessage(t *testing.T) {
+	var (
+		ctx  = context.Background()
+		req  = require.New(t)
+		ctrl = gomock.NewController(t)
+	)
+	defer ctrl.Finish()
 
-// 	userName := "user_name"
-// 	dkgRoundID := "dkg_round_id"
-// 	state := clientMocks.NewMockState(ctrl)
-// 	keyStore := clientMocks.NewMockKeyStore(ctrl)
-// 	stg := storageMocks.NewMockStorage(ctrl)
-// 	qrProcessor := qrMocks.NewMockProcessor(ctrl)
+	userName := "user_name"
+	dkgRoundID := "dkg_round_id"
+	state := clientMocks.NewMockState(ctrl)
+	keyStore := clientMocks.NewMockKeyStore(ctrl)
+	stg := storageMocks.NewMockStorage(ctrl)
 
-// 	testClientKeyPair := keystore.NewKeyPair()
-// 	keyStore.EXPECT().LoadKeys(userName, "").Times(1).Return(testClientKeyPair, nil)
+	testClientKeyPair := keystore.NewKeyPair()
+	keyStore.EXPECT().LoadKeys(userName, "").Times(1).Return(testClientKeyPair, nil)
 
-// 	sp := services.ServiceProvider{}
-// 	sp.SetLogger(logger.NewLogger(userName))
-// 	sp.SetState(state)
-// 	sp.SetKeyStore(keyStore)
-// 	sp.SetStorage(stg)
-// 	sp.SetQRProcessor(qrProcessor)
+	sp := services.ServiceProvider{}
+	sp.SetLogger(logger.NewLogger(userName))
+	sp.SetState(state)
+	sp.SetKeyStore(keyStore)
+	sp.SetStorage(stg)
 
-// 	// minimal config to make test
-// 	cfg := config.Config{
-// 		Username: userName,
-// 	}
+	// minimal config to make test
+	cfg := config.Config{
+		Username: userName,
+	}
 
-// 	clt, err := NewNode(ctx, &cfg, &sp)
-// 	req.NoError(err)
+	clt, err := NewNode(ctx, &cfg, &sp)
+	req.NoError(err)
 
-// 	t.Run("test_process_dkg_init", func(t *testing.T) {
-// 		fsm, err := state_machines.Create(dkgRoundID)
-// 		req.NoError(err)
-// 		state.EXPECT().LoadFSM(dkgRoundID).Times(1).Return(fsm, true, nil)
+	t.Run("test_process_dkg_init", func(t *testing.T) {
+		fsm, err := state_machines.Create(dkgRoundID)
+		req.NoError(err)
+		state.EXPECT().LoadFSM(dkgRoundID).Times(1).Return(fsm, true, nil)
 
-// 		senderKeyPair := keystore.NewKeyPair()
-// 		senderAddr := senderKeyPair.GetAddr()
-// 		messageData := requests.SignatureProposalParticipantsListRequest{
-// 			Participants: []*requests.SignatureProposalParticipantsEntry{
-// 				{
-// 					Username:  senderAddr,
-// 					PubKey:    senderKeyPair.Pub,
-// 					DkgPubKey: make([]byte, 128),
-// 				},
-// 				{
-// 					Username:  "111",
-// 					PubKey:    keystore.NewKeyPair().Pub,
-// 					DkgPubKey: make([]byte, 128),
-// 				},
-// 				{
-// 					Username:  "222",
-// 					PubKey:    keystore.NewKeyPair().Pub,
-// 					DkgPubKey: make([]byte, 128),
-// 				},
-// 				{
-// 					Username:  "333",
-// 					PubKey:    keystore.NewKeyPair().Pub,
-// 					DkgPubKey: make([]byte, 128),
-// 				},
-// 			},
-// 			CreatedAt:        time.Now(),
-// 			SigningThreshold: 2,
-// 		}
-// 		messageDataBz, err := json.Marshal(messageData)
-// 		req.NoError(err)
+		senderKeyPair := keystore.NewKeyPair()
+		senderAddr := senderKeyPair.GetAddr()
+		messageData := requests.SignatureProposalParticipantsListRequest{
+			Participants: []*requests.SignatureProposalParticipantsEntry{
+				{
+					Username:  senderAddr,
+					PubKey:    senderKeyPair.Pub,
+					DkgPubKey: make([]byte, 128),
+				},
+				{
+					Username:  "111",
+					PubKey:    keystore.NewKeyPair().Pub,
+					DkgPubKey: make([]byte, 128),
+				},
+				{
+					Username:  "222",
+					PubKey:    keystore.NewKeyPair().Pub,
+					DkgPubKey: make([]byte, 128),
+				},
+				{
+					Username:  "333",
+					PubKey:    keystore.NewKeyPair().Pub,
+					DkgPubKey: make([]byte, 128),
+				},
+			},
+			CreatedAt:        time.Now(),
+			SigningThreshold: 2,
+		}
+		messageDataBz, err := json.Marshal(messageData)
+		req.NoError(err)
 
-// 		message := storage.Message{
-// 			ID:         uuid.New().String(),
-// 			DkgRoundID: dkgRoundID,
-// 			Offset:     1,
-// 			Event:      string(spf.EventInitProposal),
-// 			Data:       messageDataBz,
-// 			SenderAddr: senderAddr,
-// 		}
-// 		message.Signature = ed25519.Sign(senderKeyPair.Priv, message.Bytes())
+		message := storage.Message{
+			ID:         uuid.New().String(),
+			DkgRoundID: dkgRoundID,
+			Offset:     1,
+			Event:      string(spf.EventInitProposal),
+			Data:       messageDataBz,
+			SenderAddr: senderAddr,
+		}
+		message.Signature = ed25519.Sign(senderKeyPair.Priv, message.Bytes())
 
-// 		state.EXPECT().SaveFSM(gomock.Any(), gomock.Any()).Times(1).Return(nil)
-// 		state.EXPECT().PutOperation(gomock.Any()).Times(1).Return(nil)
+		state.EXPECT().SaveFSM(gomock.Any(), gomock.Any()).Times(1).Return(nil)
+		state.EXPECT().PutOperation(gomock.Any()).Times(1).Return(nil)
 
-// 		err = clt.ProcessMessage(message)
-// 		req.NoError(err)
-// 	})
-// }
+		err = clt.ProcessMessage(message)
+		req.NoError(err)
+	})
+}
 
 func TestClient_GetOperationsList(t *testing.T) {
 	var (
@@ -156,60 +161,6 @@ func TestClient_GetOperationsList(t *testing.T) {
 	req.Len(operations, 1)
 	req.Equal(operation, operations[operation.ID])
 }
-
-// func TestClient_GetOperationPath(t *testing.T) {
-// 	var (
-// 		ctx  = context.Background()
-// 		req  = require.New(t)
-// 		ctrl = gomock.NewController(t)
-// 	)
-// 	defer ctrl.Finish()
-
-// 	userName := "test_client"
-
-// 	keyStore := clientMocks.NewMockKeyStore(ctrl)
-// 	testClientKeyPair := keystore.NewKeyPair()
-// 	keyStore.EXPECT().LoadKeys(userName, "").Times(1).Return(testClientKeyPair, nil)
-
-// 	state := clientMocks.NewMockState(ctrl)
-// 	stg := storageMocks.NewMockStorage(ctrl)
-
-// 	sp := services.ServiceProvider{}
-// 	sp.SetLogger(logger.NewLogger(userName))
-// 	sp.SetState(state)
-// 	sp.SetKeyStore(keyStore)
-// 	sp.SetStorage(stg)
-
-// 	// minimal config to make test
-// 	cfg := config.Config{
-// 		Username: userName,
-// 	}
-
-// 	clt, err := NewNode(ctx, &cfg, &sp)
-// 	req.NoError(err)
-
-// 	operation := &types.Operation{
-// 		ID:        "operation_id",
-// 		Type:      types.DKGCommits,
-// 		Payload:   []byte("operation_payload"),
-// 		CreatedAt: time.Now(),
-// 	}
-
-// 	var expectedQrPath = filepath.Join(jsonFilesDir, fmt.Sprintf("dc4bc_json_%s.json", operation.ID))
-// 	defer os.Remove(expectedQrPath)
-
-// 	state.EXPECT().GetOperationByID(operation.ID).Times(1).Return(
-// 		nil, errors.New(""))
-// 	_, err = clt.GetOperationPath(&dto.OperationIdDTO{OperationID: operation.ID})
-// 	req.Error(err)
-
-// 	state.EXPECT().GetOperationByID(operation.ID).Times(1).Return(
-// 		operation, nil)
-// 	qrProcessor.EXPECT().WriteQR(expectedQrPath, gomock.Any()).Times(1).Return(nil)
-// 	qrPath, err := clt.GetOperationQRPath(&dto.OperationIdDTO{OperationID: operation.ID})
-// 	req.NoError(err)
-// 	req.Equal(expectedQrPath, qrPath)
-// }
 
 func TestClient_ResetState(t *testing.T) {
 	var (
