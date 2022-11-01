912d0dff73835e733a61aab352b39450e157ee77
sergeywh1te <sergey.i@lido.fi>
2022-11-01 13:51:31 +0400

    some marks for signing bls execution change payload

--- pkg/wc_rotation/rotation_test.go
+++ pkg/wc_rotation/rotation_test.go
@@ -1,7 +1,7 @@
 package wc_rotation
 
 import (
-	"reflect"
+	"encoding/hex"
 	"testing"
 )
 
@@ -13,28 +13,30 @@ func TestGetSigningRoot(t *testing.T) {
 	tests := []struct {
 		name    string
 		args    args
-		want    [32]byte
+		want    string
 		wantErr bool
 	}{
 		{
 			name: "success",
 			args: args{
-				validatorIndex: 52793,
+				validatorIndex: 393395,
 			},
-			want: [32]byte{
-				177, 122, 243, 237, 155, 72, 202, 9, 3, 132, 122, 193, 247, 215, 19, 0, 4, 76, 147, 121, 183, 215, 188, 24, 249, 103, 175, 3, 160, 41, 239, 35,
-			},
+			want:    `2202563bc9261c6371fa133be05da0bd42762a7df037a1da7d69d4f756528eb0`,
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := GetSigningRoot(tt.args.validatorIndex)
+
 			if (err != nil) != tt.wantErr {
 				t.Errorf("GetSigningRoot() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if !reflect.DeepEqual(got, tt.want) {
+
+			actual := hex.EncodeToString(got[:])
+
+			if actual != tt.want {
 				t.Errorf("GetSigningRoot() got = %v, want %v", got, tt.want)
 			}
 		})
